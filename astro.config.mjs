3e0388a657e30d0967afd19e91992c1703f3bb91
Daniel Schmid <daniel@danielschmid.com>
2025-11-01 13:20:23 +0100

    Setup Bun and TypeScript path aliases

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import path from "path";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "static",
+  vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+        "@components": path.resolve("./src/components"),
+        "@layouts": path.resolve("./src/layouts"),
+        "@pages": path.resolve("./src/pages"),
+        "@assets": path.resolve("./src/assets"),
+        "@styles": path.resolve("./src/styles"),
+        "@utils": path.resolve("./src/utils"),
+        "@types": path.resolve("./src/types"),
+      },
+    },
+  },
+});
